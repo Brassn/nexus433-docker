4cb6b0a6b8f13afccb59418e45400ec7b66b03a1
Brassn <48657890+Brassn@users.noreply.github.com>
2020-04-16 17:40:05 +0200

    removed unused lines in startup script

--- src/cmd.sh
+++ src/cmd.sh
@@ -4,19 +4,6 @@ function log_info {
         echo -e $(date '+%Y-%m-%d %T')"\e[1;32m $@\e[0m"
 }
 
-#function terminate_container {
-#  kill ${spid}
-#  if [ $pid -ne 0 ]; then
-#    kill -SIGTERM "$pid"
-#    wait "$pid"
-#  fi
-#  log_info "shutting down container"
-#  exit 0
-#}
-
-
-#trap "terminate_container" SIGTERM
-
 if [ -z "${MQTT_SERVER}" ]; then
   log_info "MQTT_SERVER variable must be set, exiting"
   exit 1
@@ -26,9 +13,5 @@ while true
 do
         log_info "Starting Nexus433"
         exec nexus433 -a ${MQTT_SERVER} --verbose
-        sleep 1s
-#        export pid=${!}
-#        sleep 1s &
-#        export spid=${!}
-#        wait $spid
+        sleep 10s
 done
